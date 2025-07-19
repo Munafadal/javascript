fc6ab946e13c4d6c09b883cb1499f1375f5109d7
Muna Osman <mofadal009@gmail.com>
2025-07-19 18:01:10 +0100

    this is my javascript course from my web development bootcamp

---
+++ ch1_variables_and_datatypes/variable.js
@@ -0,0 +1,19 @@
+// var firstName = "Muna" // string
+// var lastName = "Osman" // string
+// var age = 35 // number
+// var city = "London" // string
+// var isStudent = true // boolean
+
+
+// console.log(FirstName * LastName)
+// console.log(age * 2)
+
+var $magacaKowad = 3;//declaring a variable
+// FirstName = "Ahmed" // assigning a value, initializing a variable
+// FirstName = "Muna"
+console.log(typeof $magacaKowad)
+
+
+
+
+
---
+++ introjs/script.js
@@ -0,0 +1,5 @@
+console.log("Hello, node.js!")
+console.log("Hello World")
+console.log("Hello World")
+console.log("Hello World")
+console.log("Hi!");
