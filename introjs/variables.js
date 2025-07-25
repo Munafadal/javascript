548dae5cde34785bde14d4b3ff100d99bd8f96fb
Muna Osman <mofadal009@gmail.com>
2025-07-25 22:47:28 +0100

    variables.js

---
+++ introjs/variables.js
@@ -0,0 +1,26 @@
+// var firstName = ["Muna", "Osman"]; //array
+// var lastName = "Osman" //string
+// var age = [35, 16]; // number/array
+// var isMarried = "false" //bulean
+// var county = "London" //string
+// var hobbies = ["reading", "swimming", "cycling"] //array
+// var salary = 500 //number
+// var taxrate = 0.25; //number
+// var address = {
+//     street: "street 1",
+//     city: "city 1",
+//     country: "country1"
+// }; //object
+    
+// console.log(firstName.toUpperCase());
+// console.log(firstName.toLowerCase());
+// console.log(firstName);
+// console.log(lastName)
+// console.log(salary * taxrate)
+// console.log(typeof firstName)
+
+var firstName = "Muna";
+
+console.log(firstName)
+var theTownthatIwasBornisKismayo = "mayo"
+console.log (theTownthatIwasBornisKismayo)
