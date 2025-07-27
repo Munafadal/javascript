48f00fcc8f8e1153ece4b5927b452b120729a268
Muna Osman <mofadal009@gmail.com>
2025-07-28 00:18:44 +0100

    arithmatic/

---
+++ operators/arithmatic/script.js
@@ -0,0 +1,33 @@
+// var a = 23
+// var b = 4
+
+// // addition
+// var result = a + b
+// console.log(result) // 8
+
+// // subtraction
+// var result2 = a - b
+// console.log(result2) // 2
+
+// // multiplication
+// var result3 = a * b
+// console.log(result3) // 15
+
+// // division
+// var result4 = a / b
+// console.log(result4) // 1.6666666666666667
+
+// // modulas
+// var result5 = a % b
+// console.log(result5) // 2
+
+// incriment operator
+// var age = 20
+// age++
+// console.log (age)
+
+// decriment operator
+var age = 20
+age--
+console.log (age)
+
