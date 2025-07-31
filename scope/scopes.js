c7cf097440c9c7c6098a2b4e20fb698a7fed65e6
Muna Osman <mofadal009@gmail.com>
2025-07-31 23:46:30 +0100

    scopes.js

---
+++ scope/scopes.js
@@ -0,0 +1,14 @@
+// let firstName = "Muna" //this is a global variable
+// console.log("this is my name: ", firstName) 
+
+
+// {
+//     let lastName = "Osman" //this is a local variable
+//     console.log(lastName);
+//     console.log (firstName);
+// }
+
+
+const firstName = "Muna"
+firstName = "Ali"
+console.log (firstName)
